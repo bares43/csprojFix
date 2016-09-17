4dccceef26a846e50625f97675a4a3425ad4b89e
Jan Bareš <janbares43@gmail.com>
2016-09-17 09:42:57 +0200

    init

---
+++ csprojFix/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using csprojFix.Service;
+
+namespace csprojFix {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        XML _xmlService;
+
+        public MainWindow() {
+
+            _xmlService = new XML();
+
+            InitializeComponent();
+        }
+
+        private void load_Click(object sender, RoutedEventArgs e) {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.ShowDialog();
+            path.Content = dialog.FileName;
+        }
+
+        private void find_Click(object sender, RoutedEventArgs e) {
+
+            errors.Text = string.Empty;
+
+            if (string.IsNullOrEmpty(path.Content.ToString())) {
+                MessageBox.Show("Please choose csproj file.", "Error");
+                return;
+            }
+
+            try {
+
+                var directory = System.IO.Path.GetDirectoryName(path.Content.ToString());
+
+                var xdoc = XDocument.Load(path.Content.ToString());
+
+                if (duplicated.IsChecked.HasValue && duplicated.IsChecked.Value) {
+
+                    var duplicatedFiles = _xmlService.FindDuplicatedContentFiles(xdoc);
+
+                    if (duplicatedFiles.Any()) {
+                        errors.AppendText("-- duplicated content files --");
+                        errors.AppendText(Environment.NewLine);
+                        errors.AppendText(Environment.NewLine);
+
+                        foreach (var file in duplicatedFiles) {
+                            errors.AppendText($"{file.Key} ({file.Value}x)");
+                            errors.AppendText(Environment.NewLine);
+                        }
+                        errors.AppendText(Environment.NewLine);
+                    }
+
+                    duplicatedFiles = _xmlService.FindDuplicatedCompileFiles(xdoc);
+
+                    if (duplicatedFiles.Any()) {
+                        errors.AppendText("-- duplicated compile files --");
+                        errors.AppendText(Environment.NewLine);
+                        errors.AppendText(Environment.NewLine);
+
+                        foreach (var file in duplicatedFiles) {
+                            errors.AppendText($"{file.Key} ({file.Value}x)");
+                            errors.AppendText(Environment.NewLine);
+                        }
+                        errors.AppendText(Environment.NewLine);
+                    }
+
+                }
+
+                if (missingOnDisk.IsChecked.HasValue && missingOnDisk.IsChecked.Value) {
+
+                    var missingFiles = _xmlService.FindMissingContentFilesOnDisk(xdoc, directory);
+
+                    if (missingFiles.Any()) {
+                        errors.AppendText("-- missing content files on disk --");
+                        errors.AppendText(Environment.NewLine);
+                        errors.AppendText(Environment.NewLine);
+
+                        foreach (var file in missingFiles) {
+                            errors.AppendText(file);
+                            errors.AppendText(Environment.NewLine);
+                        }
+                        errors.AppendText(Environment.NewLine);
+                    }
+
+                    missingFiles = _xmlService.FindMissingCompileFilesOnDisk(xdoc, directory);
+
+                    if (missingFiles.Any()) {
+                        errors.AppendText("-- missing compile files on disk --");
+                        errors.AppendText(Environment.NewLine);
+                        errors.AppendText(Environment.NewLine);
+
+                        foreach (var file in missingFiles) {
+                            errors.AppendText(file);
+                            errors.AppendText(Environment.NewLine);
+                        }
+                        errors.AppendText(Environment.NewLine);
+                    }
+
+                }
+
+                if (string.IsNullOrEmpty(errors.Text)) {
+                    errors.Text = "no errors found :)";
+                }
+            } catch (Exception) {
+                MessageBox.Show("Error occured.", "Error");
+            }
+
+        }
+    }
+}
---
+++ csprojFix/Service/XML.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace csprojFix.Service {
+    class XML {
+
+        public IDictionary<string,int> FindDuplicatedContentFiles(XDocument xdoc) {
+            return this.FindDuplicatedFiles(xdoc, "Content");
+        }
+
+        public IDictionary<string, int> FindDuplicatedCompileFiles(XDocument xdoc) {
+            return this.FindDuplicatedFiles(xdoc, "Compile");
+        }
+
+        public IList<string> FindMissingContentFilesOnDisk(XDocument xdoc, string basePath) {
+            return this.FindMissingFilesOnDisk(xdoc, basePath, "Content");
+        }
+
+        public IList<string> FindMissingCompileFilesOnDisk(XDocument xdoc, string basePath) {
+            return this.FindMissingFilesOnDisk(xdoc, basePath, "Compile");
+        }
+
+        private IList<string> FindMissingFilesOnDisk(XDocument xdoc, string basePath, string node) {
+            var files = xdoc
+                   .Root
+                   .Descendants()
+                   .Where(o => o.Name.LocalName == "ItemGroup")
+                   .Descendants()
+                   .Where(o => o.Name.LocalName == node)
+                   .Select(o => o.Attribute("Include").Value)
+                   .Distinct()
+                   .ToList();
+
+            var missingFiles = new List<string>();
+            
+            foreach (var file in files) {
+                if (!File.Exists(basePath + "/" + file)) {
+                    missingFiles.Add(file);
+                }
+            }
+
+            return missingFiles;
+        }
+        
+        private IDictionary<string, int> FindDuplicatedFiles(XDocument xdoc, string node) {
+            var files = xdoc
+                   .Root
+                   .Descendants()
+                   .Where(o => o.Name.LocalName == "ItemGroup")
+                   .Descendants()
+                   .Where(o => o.Name.LocalName == node)
+                   .Select(o => o.Attribute("Include").Value)
+                   .ToList()
+                   .GroupBy(o => o)
+                   .Select(group => new {
+                       File = group.Key,
+                       Count = group.Count()
+                   })
+                   .Where(o => o.Count > 1)
+                   .ToDictionary(o => o.File, o => o.Count);
+            
+            return files;
+        }
+
+    }
+}
